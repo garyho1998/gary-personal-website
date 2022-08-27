bb55ac9b1015d28e72ca5c9377da298a61e67277
garyho1998 <garyho199850@gmail.com>
2022-08-27 17:29:31 +0800

    Updates

---
+++ precache-manifest.2f3f5db43b2b38f46a68df9807695104.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf23d44ba25c22ea537a6ebd4ec19f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34ae530269970036f763",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "28f31ec7d02fabbb2ab2",
+    "url": "./static/css/main.4c56c3b8.chunk.css"
+  },
+  {
+    "revision": "34ae530269970036f763",
+    "url": "./static/js/2.d0c18166.chunk.js"
+  },
+  {
+    "revision": "d883db3707485ac5ec5663dbc0e513a7",
+    "url": "./static/js/2.d0c18166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f31ec7d02fabbb2ab2",
+    "url": "./static/js/main.42170903.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
